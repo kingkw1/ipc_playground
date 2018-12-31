15a9628e61f3d00b1f41bfb85b42b77a06dcac36
kingkw1 <king.kevin.w@gmail.com>
2018-12-30 23:30:41 -0500

    binary message encoding and decoding

--- eyeMessage.py
+++ eyeMessage.py
@@ -1,7 +1,40 @@
 import time
+import random
+import struct
 
+"""
+Conversions from: https://stackoverflow.com/questions/8751653/how-to-convert-a-binary-string-into-a-float-value
+"""
+def bin_to_float(b):
+    """ Convert 32bit binary string to a float. """
+    bf = int_to_bytes(int(b, 2), 4)  # 8 bytes needed for IEEE 754 binary64.
+    return struct.unpack('>f', bf)[0]
+
+def int_to_bytes(n, minlen=0):  # Helper function
+    """ Int/long to byte string. """
+    nbits = n.bit_length() + (1 if n < 0 else 0)  # +1 for any sign bit.
+    nbytes = (nbits+7) // 8  # Number of whole bytes.
+    b = bytearray()
+    for _ in range(nbytes):
+        b.append(n & 0xff)
+        n >>= 8
+    if minlen and len(b) < minlen:  # Zero padding needed?
+        b.extend([0] * (minlen-len(b)))
+    return bytearray(reversed(b))  # High bytes first.
+
+def float_to_bin(value):  # For testing.
+    """ Convert float to 32-bit binary string. """
+    [d] = struct.unpack(">l", struct.pack(">f", value))
+    return '{:032b}'.format(d)
+
+#def bin2float32(bin32):
+#    return struct.unpack('f', struct.pack('I', bin32))[0]
+#
+#def float2bin32(f32):
+#    return ''.join(bin(ord(c)).replace('0b', '').rjust(8, '0') for c in struct.pack('!f', f32))
+#
 class eyemessage():
-    def __init__(self, eyed, conf, x, y, d):
+    def __init__(self, eyed=None, conf=None, x=None, y=None, d=None):
         """
         Timestamp : Seconds: Int64U, Nanoseconds: Int32U
 	       Timestamp of message received
@@ -27,21 +60,31 @@ class eyemessage():
             self.ypos = y
             self.dia = d
         else:
-            self.eyed = 1
-            self.conf = 1
-            self.xpos = 1
-            self.ypos = 1
-            self.dia = 1
+            self.eyed = random.randint(0,1)
+            self.conf = random.random()
+            self.xpos = random.random()
+            self.ypos = random.random()
+            self.dia = random.randrange(1,500)
 
-    def encmssg(self):
+    def encbinmssg(self):
         mssg = format(self.timestamp_s,'064b') \
         + format(self.timestamp_ns,'032b') \
         + format(self.eyed,'01b') \
-        + format(self.conf,'032b') \
-        + format(self.xpos,'032b') \
-        + format(self.ypos,'032b') \
+        + float_to_bin(self.conf) \
+        + float_to_bin(self.xpos) \
+        + float_to_bin(self.ypos) \
         + format(self.dia,'032b')
         return mssg
 
-    def decmssg(mssg):
-        timestamp_s = mssg[1:64]
+def decmssg(mssg):
+    # a = [0,64,32,1,32,32,32,32]
+    timestamp_s = int(mssg[0:64],2)
+    timestamp_ns = int(mssg[64:96],2)
+    eyed = mssg[96:97]
+    conf = bin_to_float(mssg[97:129])
+    xpos = bin_to_float(mssg[129:161])
+    ypos = bin_to_float(mssg[161:193])
+    dia = int(mssg[193:225],2)
+    return timestamp_s, timestamp_ns, eyed, conf, xpos, ypos, dia
+
+#a = eyemessage().encbinmssg()
